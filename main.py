5153e0e7fa6a2ac005a8e634e06d3e5a90d0e1c9
Guillaume Aymé <guillaume@lenses.io>
2021-06-02 19:12:56 +0200

    update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import threading, time
 from kafka import KafkaAdminClient, KafkaProducer
 from faker import Faker
+from random import uniform
 import random
 
 
@@ -19,22 +20,39 @@ class Producer(threading.Thread):
 
 
   def run(self):
-    producer = KafkaProducer(bootstrap_servers='35.180.29.238:9092')
+    producer = KafkaProducer(bootstrap_servers='13.36.209.227:9092')
     print("about to send to Kafka")
 
+    district_list = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th"]
+
+    def newpoint():
+      return uniform(-180,180), uniform(-90, 90)
+
+ 
 
     while not self.stop_event.is_set():
       print("sending event...")
       fake = Faker()
-      agent = {
-        "name": fake.name(),
-        "disavowed": random.randint(0, 1)
+      passengerId = random.randint(0, 1000000)
+      key = {
+        "account_id" : passengerId
+      }
+      
+      payload = {
+        "passenger_name": fake.name(),
+        "passenger_id": passengerId,
+        "pickup_coordinates" : newpoint(),
+        "destination_coordinates": newpoint(),
+        "pickup_district": random.choice(district_list),
+        "destination_district": random.choice(district_list)
       }
 
       # convert into JSON:
-      agentJson = json.dumps(agent)
-      b = bytes(agentJson, 'utf-8')
-      producer.send('NOC_LIST', b)
+      payloadJson = json.dumps(payload)
+      keyJson = json.dumps(key)
+      b = bytes(payloadJson, 'utf-8')
+      k = bytes(keyJson, 'utf-8')
+      producer.send('passenger_requests', value=b, key=k)
       time.sleep(3)
 
     producer.close()
